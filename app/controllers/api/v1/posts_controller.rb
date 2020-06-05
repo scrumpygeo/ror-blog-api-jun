7446598c5a2980452f144afb5ff175b3ed9b5014
scrumpygeo <scrumpygeo@yahoo.co.uk>
2020-06-05 18:56:14 +0100

    show post

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,5 +1,17 @@
 class Api::V1::PostsController < Api::V1::BaseController
+    before_action :set_post, only: [:show]
+
     def index
       @posts = policy_scope(Post)
+    end
+
+    def show 
+    end
+
+    private
+
+    def set_post
+        @post = Post.find(params[:id])
+        authorize @post
     end
   end
