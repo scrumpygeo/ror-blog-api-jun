4b19cce99b66d6d34f3458c8d6c8ab796e69447b
scrumpygeo <scrumpygeo@yahoo.co.uk>
2020-06-19 20:30:26 +0100

    register and login with api

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,10 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
       resources :posts, only: [ :index, :show, :update,:create, :destroy ]
+      devise_scope :user do
+        post "sign_up", to: "registrations#create"
+        post "sign_in", to: "sessions#create"
+      end
     end
   end
 end
