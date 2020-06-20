208f15e1b3d0a24d8191e02d71ac22b80514b216
scrumpygeo <scrumpygeo@yahoo.co.uk>
2020-06-20 09:25:31 +0100

    fix typo in cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,15 +1,24 @@
-Rails.application.config.middleware.insert_before 0, Rack::Cors, debug: true, logger: (-> { Rails.logger }) do
-  allow do
-    origins '*'
+class Application < Rails::Application
+  # Initialize configuration defaults for originally generated Rails version.
+  config.load_defaults 6.0
 
-    resource '/cors',
-      :headers => :any,
-      :methods => [:post],
-      :max_age => 0
+  # Settings in config/environments/* take precedence over those specified here.
+  # Application configuration should go into files in config/initializers
+  # -- all .rb files in that directory are automatically loaded.
 
-    resource '*',
-      :headers => :any,
-      :methods => [:get, :post, :delete, :put, :patch, :options, :head],
-      :max_age => 0
+  # Only loads a smaller set of middleware suitable for API only apps.
+  # Middleware like session, flash, cookies can be added back manually.
+  # Skip views, helpers and assets when generating a new resource.
+  config.api_only = true
+
+  config.middleware.insert_before 0, "Rack::Cors" do
+    allow do
+      origins '*'
+      resource(
+        '*',
+        headers: :any,
+        methods: [:get, :patch, :put, :delete, :post, :options]
+        )
+    end
   end
 end
