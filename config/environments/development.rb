2c61be6e198d0bc6ec55ccfe2e998ed2221dfe49
scrumpygeo <scrumpygeo@yahoo.co.uk>
2020-06-20 21:34:35 +0100

    sort cors

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,5 +1,5 @@
 Rails.application.configure do
-  config.action_mailer.default_url_options = { host: "http://localhost:3000" }
+  # config.action_mailer.default_url_options = { host: "http://localhost:3000" }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
@@ -61,6 +61,6 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
-
-  config.hosts << 'http://localhost:3000'
+  
+  config.hosts << "localhost"
 end
